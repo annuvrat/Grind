5735049253ced5cca799ad0caa43425842769906
Annuvrat sharma <115866889+annuvrat@users.noreply.github.com>
2024-10-26 14:57:11 +0530

    Create 2D array matrix .java

---
+++ 2D array matrix .java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the elements of the 3x3 matrix:");
+
+        // Input for matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Output the matrix
+        System.out.println("The matrix is:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
