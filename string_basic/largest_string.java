ac41e4e6ef8b6c94717e6c86c459c09c8daa6978
Annuvrat sharma <115866889+annuvrat@users.noreply.github.com>
2024-11-09 16:31:53 +0530

    Create largest_string.java

---
+++ string_basic/largest_string.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String fruit[] = {"apple", "mango", "banana"};
+        
+        String largest = fruit[0];
+        for (int i = 1; i < fruit.length; i++) {
+            if (largest.compareTo(fruit[i]) < 0) {
+                largest = fruit[i];
+            }
+        }
+        
+        System.out.println(largest);
+    }
+}
