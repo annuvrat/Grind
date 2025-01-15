99c66a4d3836c20ba3e8f0a196457073f55ce4d1
Annuvrat sharma <115866889+annuvrat@users.noreply.github.com>
2025-01-15 22:40:20 +0530

    Update longest common prefix.java

--- EASY/longest common prefix.java
+++ EASY/longest common prefix.java
@@ -2,7 +2,7 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
 
         if (strs == null || strs.length == 0) return "";
-
+ 
        
         for (int index = 0; index < strs[0].length(); index++) {
             char currentChar = strs[0].charAt(index);
