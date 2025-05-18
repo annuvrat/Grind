fdce0b0938190eca350c36836aab525c3b4d4f71
Annuvrat <115866889+annuvrat@users.noreply.github.com>
2025-05-18 11:21:05 +0530

    Create array reverse.java

---
+++ array reverse.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Original array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        // Reverse the array
+        int n = arr.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+
+        System.out.println("\nReversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
