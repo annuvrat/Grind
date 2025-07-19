4d67881fe89dd80d744032334f7e3d137d24fab9
Annuvrat <115866889+annuvrat@users.noreply.github.com>
2025-07-19 23:14:18 +0530

    Create valid palindrome.java

---
+++ stack/valid palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();  
+    }
+}
+
